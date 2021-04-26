9c6ab5927f0970e57a7f8afa407fd7799a3d097c
Camilapech <72808203+Camilapech@users.noreply.github.com>
2021-04-26 11:29:21 -0500

    Video 3 del 26 de abril clase

--- Actividades/script.js
+++ Actividades/script.js
@@ -1,17 +1,69 @@
-// var n1 = 1
-// var n2 = 3
+var postres = localStorage.getItem('postres')
 
-// //función con parámetros declarada//
-
-// function sumar(n1, n2){
-//     alert(n1 + n2)
-// }
-// //función mandada a llamar//
-// sumar(n1, n2)
-
-function sumar() {
-    var n1 = parseInt(prompt("Ingresa un número"))
-    var n2 = parseInt(prompt("Ingresa otro número"))
-    
-    n1 === n2 ? alert("PEZ") : alert(n1 + n2)
+if(!postres) {
+    localStorage.setItem('peliculas', [])
 }
+
+function sumar () {
+    var n1 = Number ( prompt("Ingresa un numero") )
+    var n2 = Number ( prompt("Ingresa otro numero") )
+    if (n1 === 2 && n2 === 2) {
+        alert("Pez")
+    } else {
+        alert(n1 + n2)
+    }
+}
+
+var verFormulario = false
+
+function mostrarFormulario() {
+    var formulario = document.getElementById('crearFormulario')
+    var cards = document.getElementById('cards')
+    var crearBtn = document.getElementById('crearBtn')
+
+    if (!verFormulario) {
+        formulario.style.display = 'block'
+        cards.style.display = 'none'
+        verFormulario = true
+        crearBtn.innerText = 'Cancelar' 
+    } else {
+        formulario.style.display = 'none'
+        cards.style.display = 'block'
+        verFormulario = false
+        crearBtn.innerText = 'Crear'
+    }
+    // Instrucciones para desaparecer a #cards
+
+    // Cambiar el texto del boton crear por la palabra Cancelar
+    // Si oprimo una vez el boton crear, muestra el formulario y desaparecen
+    // las cards, si lo oprimo nuevamente, muestra las cards y desaparece
+    // el formulario
+}
+
+var crearFormulario = document.getElementById('crearFormulario')
+crearFormulario.addEventListener('submit', function(e) {
+    e.preventDefault()
+    
+    var formulario = new FormData(crearFormulario)
+    var postre = {
+        'titulo' : formulario.get('formTitle'),
+        'imagen' : formulario.get('formImage'),
+        'descripcion' : formulario.get('formDescripcion')
+    } 
+    if (!postres) {
+        var listadodepostres = [
+            postre
+        ]
+
+        localStorage.setItem('postres', JSON.stringify(listadodepostres))
+    } else {
+        var listadodepostres = JSON.parse(postres)
+        listadodepostres.push(postre)
+        localStorage.setItem('postres', JSON.stringify(listadodepostres))
+    }
+
+    crearFormulario.reset()
+    mostrarFormulario()
+} )
+
+// sumar(num1, num2)
