9437fe3fdab1b60c081c5abaf42a0a1c948855fa
Camilapech <72808203+Camilapech@users.noreply.github.com>
2021-04-29 22:16:18 -0500

    validación y guardado de formulario

--- Proyecto/formulario.js
+++ Proyecto/formulario.js
@@ -8,3 +8,30 @@ for (var i = 0; i < inputs.length; i++) {
         }
     });
 }
+
+
+var getData = function (){
+    var name = document.getElementById("name").value;
+    var correo = document.getElementById("correo").value;
+    var tel = document.getElementById("tel").value;
+    if(name == "") {
+        document.getElementById("name").focus();
+    }else {
+        if (correo == "") {
+            document.getElementById("correo").focus();
+        } else {
+            if (tel == "") {
+                document.getElementById("tel").focus();
+            } else {
+                console.log(name+correo+tel)
+                document.getElementById("name").value = "";
+                document.getElementById("correo").value = "";
+                document.getElementById("tel").value = "";
+            }
+        }
+    }
+}
+
+
+
+
